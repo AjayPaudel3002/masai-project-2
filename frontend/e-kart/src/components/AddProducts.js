32127afce503fe583922e33cf3ff135be6967ab6
Ajaypaudel3002 <b.ajpaudel@gmail.com>
2019-10-23 12:36:57 +0530

    components created

---
+++ frontend/e-kart/src/components/AddProducts.js
@@ -0,0 +1,22 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import axios from "axios"
+import { Router as BrowserRouter, Link } from "react-router-dom"
+// import queryString from "query-string"
+
+export default class AddProducts extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return (
+            <div>
+                "Ajay"
+            </div>
+        )
+    }
+}
