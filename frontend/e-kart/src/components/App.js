2b11c516feb6b8b6d3002459eb532f4de5198ca7
Ajaypaudel3002 <b.ajpaudel@gmail.com>
2019-10-26 18:36:59 +0530

    modification in app.js

--- frontend/e-kart/src/components/App.js
+++ frontend/e-kart/src/components/App.js
@@ -4,27 +4,29 @@ import {Router as BrowserRouter,Link,Route,Switch} from "react-router-dom"
 import Home from "./Home"
 import Orders from "./Orders"
 import Login from "./Login"
-import AddProducts from "./AddProducts"
-import ViewProducts from "./ViewProducts"
-import Products from "./Products"
-// import Register from "./Register.js"
+import Register from "./Register.js"
 // import CompanyRegister from "CompanyRegister"
+import {LoggedInRoute, PrivateRoute, PrivateAdminRoute, PrivateCustomerRoute, PrivateVendorRoute} from './Routes'
 
 export default class App extends React.Component{
     render(){
         return(
             <React.Fragment>
-                    <Route path = "/" exact render = {(props)=><Home {...props}></Home>}></Route>
-                    <Route path = "/login" render = {(props)=><Login {...props}></Login>}></Route>
+                    {/* <Route path = "/" exact render = {(props)=><Home {...props}></Home>}></Route> */}
+                    <Route path = "/" exact component={Home}/>
+                    {/* <Route path = "/login" render = {(props)=><Login {...props}></Login>}></Route> */}
+                    <Route path = "/login/:person" component={Login}/>
                     {/* <Route path = "/company_register" render ={(props)=><CompanyRegister> {...props}</CompanyRegister>}></Route> */}
                     {/* <Route path = "/register" render = {(props)=><Register {...props}></Register>}></Route> */}
+                    <PrivateRoute path = "/register" component={Register}/>
                     {/* <Route path = "/all_logins" render = {(props)=><AllLogins> {...props}</AllLogins>}></Route> */}
-                    <Route path = "/brands/:brand/:location" render = {(props)=><Products {...props}></Products>}></Route>
+                    {/* <Route path = "/products" render = {(props)=><Products> {...props}</Products>}></Route> */}
                     {/* <Route path = "/buy_products/:product_id" render = {(props)=><BuyProducts> {...props}</BuyProducts>}></Route> */}
                     {/* <Route path = "/cart" render = {(props)=><Cart> {...props}</Cart>}></Route> */}
-                    <Route path = "/orders" render = {(props)=><Orders {...props}></Orders>}></Route>
-                    <Route path = "/add_products/:vendor_id" render = {(props)=><AddProducts{...props}></AddProducts>}></Route>
-                    <Route path = "/view_products/:vendor_id" render = {(props)=><ViewProducts {...props}></ViewProducts>}></Route>
+                    {/* <Route path = "/orders" render = {(props)=><Orders {...props}></Orders>}></Route> */}
+                    <PrivateCustomerRoute path = "/orders" component={Orders}/>
+                    {/* <Route path = "/add_products/:vendor_id" render = {(props)=><AddProducts> {...props}</AddProducts>}></Route> */}
+                    {/* <Route path = "/view_products/:vendor_id" render = {(props)=><ViewProducts> {...props}</ViewProducts>}></Route> */}
                     {/* <Route path = "/vendor_current_orders/:vendor_id" render = {(props)=><VendorCurrentOrders> {...props}</VendorCurrentOrders>}></Route> */}
                     {/* <Route path = "/vendor_past_orders/:vendor_id" render = {(props)=><VendorPastOrders> {...props}</VendorPastOrders>}></Route> */}
                     {/* <Route path = "/admin_past_order/" render = {(props)=><AdminPastOrders> {...props}</AdminPastOrders>}></Route> */}
